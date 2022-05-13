ec2f97fa41427bb2ec571d621b2323553fd057f3
Oscar <boscar.vs98@gmail.com>
2022-05-13 23:52:35 +0200

    Fix Almanax Task "embed" missing

--- cogs/Dofus/Dofus.py
+++ cogs/Dofus/Dofus.py
@@ -57,7 +57,7 @@ class Dofus(commands.Cog):
     async def almanax_task(self):
         logging.info("Almanax tasks run")
         await self.almanax_message.delete()
-        self.almanax_message = await self.almanax_channel.send(AlmanaxView.data_to_embed(await Almanax_scraper.get_almanax()))
+        self.almanax_message = await self.almanax_channel.send(embed = AlmanaxView.data_to_embed(await Almanax_scraper.get_almanax()))
 
     @almanax_task.before_loop
     async def before_almanax_task(self):
