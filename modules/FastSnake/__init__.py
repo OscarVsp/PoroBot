d0b6c2d3f2245484e4327ce3732dbb2f6706615f
OscarVsp <boscar.vs98@gmail.com>
2022-08-25 17:12:22 +0200

    revert init

---
+++ modules/FastSnake/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import disnake
+
+from .Assets import *
+from .ChoicesView import *
+from .ConfirmationView import *
+from .Embed import *
+from .MemberSelectionView import *
+from .ShadowMember import *
+from .Views import *
