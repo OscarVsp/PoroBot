d605a4c4f3d5671c2032593358353ec82b802160
OscarVsp <boscar.vs98@gmail.com>
2022-09-04 16:40:18 +0200

    really fixed

--- cogs/Dofus/Dofus.py
+++ cogs/Dofus/Dofus.py
@@ -21,7 +21,7 @@ class Dofus(commands.Cog):
         self.bot: commands.InteractionBot = bot
         self.scraper = Almanax_scraper()
         self.almanax_message: disnake.Message = None
-        self.almanax_channel = os.getenv("ALMANAX_CHANNEL", None)
+        self.almanax_channel = self.bot.get_channel(int(os.getenv("ALMANAX_CHANNEL", None)))
         self.almanax_task.add_exception_type(asyncpg.PostgresConnectionError)
         self.almanax_task.start()
 
